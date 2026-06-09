a74cb403744342c1fabc03362420c95d6937c9fb
bhavithraba28-cmd <bhavithraba28@gmail.com>
2026-06-09 14:28:50 +0530

    Add files via upload
    simple code to understand pointer

---
+++ pointer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 100;
+    int *p = &a;
+
+    cout << "a = " << a << endl;
+    cout << "Address of a = " << &a << endl;
+    cout << "Pointer p = \n" << p;
+    cout << "Value using pointer = " << *p << endl;
+
+    return 0;
+}
