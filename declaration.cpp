749b44c040e94a3355f152aa197e4016387f66d0
bhavithraba28-cmd <bhavithraba28@gmail.com>
2026-06-09 14:09:19 +0530

    Add files via upload
    to understand declaraion for data types

---
+++ declaration.cpp
@@ -0,0 +1,28 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+
+{
+        auto status = "online";
+        int id = 1;
+        string name = "BHAVITHRA";
+        char grade = 'A';
+        float weight = 52.5;
+        double height = 5.765;
+        bool eligible = true;
+
+        cout<< "status" << ":" <<status <<endl;
+        cout<< "id" << ":" <<id <<endl;
+        cout<< "name"  << ":"<< name <<endl;
+        cout<< "grade" << ":" <<grade <<endl;
+        cout<< "weight" <<":" <<weight <<endl;
+        cout<< "height"<< ":"<<height <<endl;
+        cout<<"eligible"<< ":"<< eligible <<endl;
+
+
+        return 0;
+
+
+}
