df7edcd098eb3f9d70cad5cfa3a6b9c0743e17b9
bhavithraba28-cmd <bhavithraba28@gmail.com>
2026-06-09 13:47:18 +0530

    Add files via upload
    code for assignment operation such as +,-,=,*,/.

---
+++ assignment.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    {
+    auto status = "online";
+        int id = 1;
+        string name = "BHAVITHRA";
+        char grade = 'A';
+        float weight = 52.5;
+        double height = 5.765;
+        bool eligible = true;
+
+        cout<< "status" << ":" <<status <<endl;
+        cout<< "id" << ":" <<id <<endl;
+        cout<< "name"  << ":"<< name <<endl;
+        cout<< "grade" << ":" <<grade <<endl;
+        cout<< "weight" <<":" <<weight <<endl;
+        cout<< "height"<< ":"<<height <<endl;
+        cout<<"eligible"<< ":"<< eligible <<endl;
+    }{
+    int a =5;
+    double b=4.145;
+    float c= 2.5;
+    string d = "integral ";
+    
+    a += 12;
+    b *= 3;
+    c /= 100;
+    d += "values";
+
+    cout<< "a=" <<"" <<a<<endl;
+    cout<< "b=" <<""<<b<<endl;
+    cout<< "c=" <<"" <<c<<endl;
+    cout<< d <<endl;
+    }{
+    int a=10,c=30;
+    double b= 5.4;
+    int data = a+b*c;
+    float div = c/a;
+    char  e= 'S';
+    string first = "bhavithra";
+    string second ="bujulu";
+
+cout << div <<endl;
+    cout << data <<endl;
+    cout << int(e) <<endl;
+    cout << first + " " + second <<endl;
+    }
+    int x=18, y=10;
+    cout << (x>y)<< endl;
+    cout <<(x==y)<< endl;
+    cout<< (x<=y)<<endl;
+    cout <<(x<=10 && y>=10) << endl;
+    cout<< (x==18 || y<=5)<< endl;
+    cout<< (x!=y);
+        int m= 19, n= 10;
+        int min = (m>n)? 1 : 9;
+        cout <<min;
+    
+
+    return 0;
+
+
+}
