5ee2f623a9914b567c55ac12585f240b29cc2fe6
bhavithraba28-cmd <bhavithraba28@gmail.com>
2026-06-09 13:35:54 +0530

    Add files via upload

---
+++ arithmetic.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    int a=10,c=30;
+    double b= 5.4;
+    int data = a+b*c;
+    float div = c/a;
+    char  e= 'S';
+    string first = "bhavithra";
+    string second ="bujulu";
+
+cout << div <<endl;
+    cout << data <<endl;
+    cout << int(e) <<endl;
+    cout << first + " " + second <<endl;
+    return 0;
+}
