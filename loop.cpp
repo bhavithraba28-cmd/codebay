5978f1a7071ea180041c389d7b28a2c396f51f6f
bhavithraba28-cmd <bhavithraba28@gmail.com>
2026-06-09 14:19:30 +0530

    Add files via upload
    loop code for if, else if

---
+++ loop.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    int x=1;
+    do{
+        cout<< x <<"\n";
+      x++;
+    }while(x<=5); 
+    {
+        for(int y=1;y<=5;y++)
+        if (y==10)
+        cout<<"hai"<<endl;
+        else if(y!=5)
+        cout<<"bye"<<endl;
+        else
+        cout <<"goodbye"<<endl;
+    }
+    return 0;
+
+}
