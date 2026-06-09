04d4318af9f78f451c2ec99058ca25571f7deae2
bhavithraba28-cmd <bhavithraba28@gmail.com>
2026-06-09 21:25:00 +0530

    Create relation.cpp

---
+++ BHAVI_MOHAN/relation.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    int x=18, y=10;
+    cout << (x>y)<< endl;
+    cout <<(x==y)<< endl;
+    cout<< (x<=y)<<endl;
+    cout <<(x<=10 && y>=10) << endl;
+    cout<< (x==18 || y<=5)<< endl;
+    cout<< (x!=y);
+    return 0;
+
+}
+/*simple code for relation operation*/
