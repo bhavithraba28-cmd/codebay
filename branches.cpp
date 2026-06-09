684337fece3a3a55ad7038dec37a5bb9440dcd05
bhavithraba28-cmd <bhavithraba28@gmail.com>
2026-06-09 13:55:15 +0530

    Add files via upload
    to understand code for branches in  conditonal statement like if , if else,switch.

---
+++ branches.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int maximum, x = 11, y = 10;
+
+    if (x < y)
+        maximum = 2;
+    else if (y == x)
+        maximum = 0;
+    else
+        maximum = 1;
+
+    switch(maximum)
+    {
+        case 1:
+            cout << "x is higher value" << endl;
+            break;
+        case 2:
+            cout << "y is higher value" << endl;
+            break;
+        default:
+            cout << "x and y are same value" << endl;
+    }
+
+    return 0;
+}
